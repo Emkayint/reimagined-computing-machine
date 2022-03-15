5ead5484a7ff59f8f8426db7d7de30dca115c58d
Martin <mkmartinoes@gmail.com>
2022-03-15 19:19:54 +0300

    ft: draw parallel rectangles

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,18 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+// ctx.fillStyle = "green";
+// ctx.fillRect(10, 10, 150, 100);
+
+function draw(){
+    let canvas = document.getElementById("canvas");
+    if(canvas.getContext){
+        let ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200, 0, 0';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, .5';
+        ctx.fillRect(30, 30, 50, 50);
+    }
+}
